932e24b2cdf0d2ad6735c662e5ccf78cb25df02e
yeonjae <pyj3740@gmail.com>
2024-06-29 15:04:37 +0900

    주석 추가- 최종

--- 0629.py
+++ 0629.py
@@ -3,10 +3,10 @@
 text = 'try hello python'
 # answer = ''
 
-# for i, t in enumerate(text):
-#     if i%2 == 0:
+# for i, t in enumerate(text): # 입력 받은 문자를 순서대로 반복
+#     if i%2 == 0: # 인덱스가 짝수일 경우 대문자
 #         answer += t.upper()
-#     else:
+#     else: # 아니면 소문자
 #         answer += t.lower()
 
 # answer = [t.upper() if i%2 == 0 else t.lower() for i, t in enumerate(text)]
