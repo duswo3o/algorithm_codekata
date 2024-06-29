962c01097f9a7759ae423aeb0e8059c613810170
yeonjae <pyj3740@gmail.com>
2024-06-29 15:33:27 +0900

    이상한 문자 만들기

--- 0629.py
+++ 0629.py
@@ -1,22 +1,6 @@
 # 이상한 문자 만들기
 # 1차 제출 실패 = 공백을 기준의로 단어의 짝수와 홀수를 바꿔줘야 함
 
-text = 'try hello python'
-
-# n = 0
-# answer = ''
-# for i in text:
-#     if i == ' ':
-#         answer+=i
-#         n = 0
-#     else:
-#         if n % 2 == 0:
-#             answer += i.upper()
-#             n+=1
-#         else:
-#             answer += i.lower()
-#             n+=1
-
 def solution(text):
     n = 0 # 공백을 기준으로 각 단어의 인덱스
     answer = '' 
@@ -37,4 +21,5 @@ def solution(text):
                 n+=1
     return answer
 
-print(solution(text))
+# text = 'try hello python'
+# print(solution(text))
