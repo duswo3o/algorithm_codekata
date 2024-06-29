2a4c25c911c8725f526bce68f28e0baceef6cf3b
yeonjae <pyj3740@gmail.com>
2024-06-29 15:23:13 +0900

    공백을 기준으로 단어를 나누는 코드로 변경

--- 0629.py
+++ 0629.py
@@ -1,21 +1,20 @@
 # 이상한 문자 만들기
+# 1차 제출 실패 = 공백을 기준의로 단어의 짝수와 홀수를 바꿔줘야 함
 
 text = 'try hello python'
-# answer = ''
 
-# for i, t in enumerate(text): # 입력 받은 문자를 순서대로 반복
-#     if i%2 == 0: # 인덱스가 짝수일 경우 대문자
-#         answer += t.upper()
-#     else: # 아니면 소문자
-#         answer += t.lower()
+n = 0
+answer = ''
+for i in text:
+    if i == ' ':
+        answer+=i
+        n = 0
+    else:
+        if n % 2 == 0:
+            answer += i.upper()
+            n+=1
+        else:
+            answer += i.lower()
+            n+=1
 
-# answer = [t.upper() if i%2 == 0 else t.lower() for i, t in enumerate(text)]
-
-# print(''.join(answer))
-
-
-def solution(text):
-    answer = [t.upper() if i%2 == 0 else t.lower() for i, t in enumerate(text)]
-    return ''.join(answer)
-
-print(solution(text))
+print(answer)
