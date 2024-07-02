c33a179eb65eac84047b0c0a60169976370799b4
yeonjae <pyj3740@gmail.com>
2024-07-02 09:15:31 +0900

    최소직사각형

--- 0702.py
+++ 0702.py
@@ -1,22 +1,11 @@
 # 최소직사각형
 
-sizes = [[60, 50], [30, 70], [60, 30], [80, 40]]
-
-width =  []
-length = []
-
-for i in sizes:
-    width.append(max(i))
-    length.append(min(i))
-
-print(max(width) * max(length))
-
 def solution(sizes):
-    width =  []
-    length = []
+    width =  [] # 가로길이 리스트
+    length = [] # 세로길이 리스트
 
     for i in sizes:
-        width.append(max(i))
-        length.append(min(i))
-
+        width.append(max(i)) # 긴 쪽만 가로 길이에 추가
+        length.append(min(i)) # 짧은 쪽만 세로 길이에 추가
+    # 가로 리스트와 세로 리스트의 가장 큰 값을 곱해서 리턴
     return max(width) * max(length)
