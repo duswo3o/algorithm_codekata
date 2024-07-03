f4ae6944d9f14063f4f4f500b93c984636affeb6
yeonjae <pyj3740@gmail.com>
2024-07-03 09:48:05 +0900

    Z보다 작은 알파벳애 대한 처리 추가

--- 0703.py
+++ 0703.py
@@ -1,6 +1,6 @@
 # 시저 암호
 
-s = "yY"
+s = "aY"
 n = 2
 
 def solution(s, n):
@@ -8,11 +8,10 @@ def solution(s, n):
     for i in s:
         if i == " ":
             answer.append(" ")
-        elif (ord(i)+n > ord('z')) or (ord(i)+n >= ord('Z')):
+        elif (ord(i) <= ord('Z') and ord(i)+n >= ord('Z')) or (ord(i)+n >= ord('z')):
             answer.append(chr(ord(i)-26+n))
         else:
             answer.append(chr(ord(i)+n))
     return ''.join(answer)
 
-print(solution(s,n))
-    
+print(solution(s,n)) 
