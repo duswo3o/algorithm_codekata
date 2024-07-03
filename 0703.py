db5d4e60a60df6b4e1bf9074d8c3261ce8957d51
yeonjae <pyj3740@gmail.com>
2024-07-03 09:52:22 +0900

    ord(i)+n과 z의 아스키코드 비교연산자에서 = 삭제

--- 0703.py
+++ 0703.py
@@ -1,14 +1,14 @@
 # 시저 암호
 
-s = "aY"
-n = 2
+s = "aAzZ"
+n = 25
 
 def solution(s, n):
     answer = []
     for i in s:
         if i == " ":
             answer.append(" ")
-        elif (ord(i) <= ord('Z') and ord(i)+n >= ord('Z')) or (ord(i)+n >= ord('z')):
+        elif (ord(i) <= ord('Z') and ord(i)+n > ord('Z')) or (ord(i)+n > ord('z')):
             answer.append(chr(ord(i)-26+n))
         else:
             answer.append(chr(ord(i)+n))
