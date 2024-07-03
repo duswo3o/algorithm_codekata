f7ffb38f37972efd86994340a064eca895cb262d
yeonjae <pyj3740@gmail.com>
2024-07-03 09:14:44 +0900

    z에 대한 처리 추가

--- 0703.py
+++ 0703.py
@@ -1,14 +1,17 @@
 # 시저 암호
 
-s = "AB"
+s = "zZ"
 n = 1
 
 answer = []
 for i in s:
     if i == " ":
         answer.append(" ")
+    elif (i == 'z') or (i == 'Z'):
+        answer.append(chr(ord(i)-26+n))
     else:
         answer.append(chr(ord(i)+n))
+
 
 print(''.join(answer))
     
