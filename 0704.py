e173baef36f0266add6b280bfa8a6a58dab33067
yeonjae <pyj3740@gmail.com>
2024-07-04 09:16:30 +0900

    1차에서 테케 실패하고 num_dict를 함수 안에 넣고 리턴값을 정수로 변경

--- 0704.py
+++ 0704.py
@@ -1,12 +1,13 @@
 # 숫자 문자열과 영단어
 
-num_dict = ["zero", "one", "two", "three", "four",
-            "five", "six", "seven", "eight", "nine"]
 s = "one4seveneight"
 
 def solution(s):
+    num_dict = ["zero", "one", "two", "three", "four",
+                "five", "six", "seven", "eight", "nine"]
+    
     for i in range(10):
         s = s.replace(num_dict[i], f"{i}")
-    return s
+    return int(s)
 
 print(solution(s))
