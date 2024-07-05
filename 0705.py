622b92b164819481830cac730c8d12fc69aa9c7b
yeonjae <pyj3740@gmail.com>
2024-07-05 11:55:23 +0900

    2차 제출 후 통과

--- 0705.py
+++ 0705.py
@@ -1,17 +1,18 @@
 # 문자열 내 마음대로 정렬하기
 
-strings = ["sun", "bed", "car"]
-n = 1
+strings = ["abce", "abcd", "cdx"]
+n = 2
 
 def solution(strings, n):
     new_sort = {}
 
     for i in strings:
-        new_sort[i[n]] = i
+        if i[n] in new_sort.keys():
+            new_sort[i[n]].append(i)
+            new_sort[i[n]].sort()
+        else:    
+            new_sort[i[n]] = [i]
 
-    return [new_sort[i] for i in sorted(new_sort.keys())]
-
-    # answer = [new_sort[i] for i in sorted(new_sort.keys())]
-    # print(answer)
+    return sum([new_sort[i] for i in sorted(new_sort.keys())], [])
 
 print(solution(strings,n))
