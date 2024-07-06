f2621bf420b8efaac2296c6298407941f6f67129
yeonjae <pyj3740@gmail.com>
2024-07-06 16:09:38 +0900

    한 번에 통과 했을 때의 짜릿함이란,,,, 신난다..

--- 0706.py
+++ 0706.py
@@ -4,6 +4,11 @@ array = [1, 5, 2, 6, 3, 7, 4]
 commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
 ## [5,6,3]
 
-for i in commands:
-    # print(i)
-    print(sorted(array[i[0]-1:i[1]])[i[2]-1])
+# for i in commands:
+#     # print(i)
+#     print(sorted(array[i[0]-1:i[1]])[i[2]-1])
+
+def solution(array, commands):
+    return [sorted(array[i[0]-1:i[1]])[i[2]-1] for i in commands]
+
+print(solution(array, commands))
