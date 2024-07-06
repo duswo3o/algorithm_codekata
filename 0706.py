272ffe811b691ffae70df382639a01f75d93bbcd
yeonjae <pyj3740@gmail.com>
2024-07-06 16:11:27 +0900

    K번째 수

--- 0706.py
+++ 0706.py
@@ -9,6 +9,7 @@ commands = [[2, 5, 3], [4, 4, 1], [1, 7, 3]]
 #     print(sorted(array[i[0]-1:i[1]])[i[2]-1])
 
 def solution(array, commands):
+    # commands 리스트를 반복해서 돌면서 array를 슬라이싱 한 후 sorted를 사용해서 정렬 -> k번째의 수출력
     return [sorted(array[i[0]-1:i[1]])[i[2]-1] for i in commands]
 
 print(solution(array, commands))
