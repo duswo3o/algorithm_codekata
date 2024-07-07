a85398584280085a03fec68bb382db2a132ec5db
yeonjae <pyj3740@gmail.com>
2024-07-07 15:16:21 +0900

    1차 제출 : 한 번만에 통과하는건 역시나 짜릿하군

--- 0707.py
+++ 0707.py
@@ -3,9 +3,18 @@
 numbers = [2,1,3,4,1] # return [2,3,4,5,6,7]
 
 a = set()
-# a.add(1)
+
 for i in range(len(numbers)-1):
     for j in numbers[i+1:]:
         a.add(numbers[i] + j)
 
 print(sorted(list(a)))
+
+def solution(numbers):
+    a = set()
+
+    for i in range(len(numbers)-1):
+        for j in numbers[i+1:]:
+            a.add(numbers[i] + j)
+
+    return sorted(list(a))
