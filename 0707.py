c6fb2fb5eb78f523ce00e1183991d5b50f9b8d7b
yeonjae <pyj3740@gmail.com>
2024-07-07 15:19:20 +0900

    두 개 뽑아서 더하기

--- 0707.py
+++ 0707.py
@@ -11,10 +11,10 @@ for i in range(len(numbers)-1):
 print(sorted(list(a)))
 
 def solution(numbers):
-    a = set()
+    a = set() # 중복제거를 위한 집합
 
-    for i in range(len(numbers)-1):
-        for j in numbers[i+1:]:
-            a.add(numbers[i] + j)
+    for i in range(len(numbers)-1): # 첫 번째부터 마지막 전까지 반복
+        for j in numbers[i+1:]: # 해당 인덱스 뒤의 리스트
+            a.add(numbers[i] + j) # 해당 인덱스의 값과 그 이후에 있는 값들을 하나씩 반복하며 더하여 집합에 추가
 
-    return sorted(list(a))
+    return sorted(list(a)) # 집합을 리스트로 바꾸어주고 오름차순 정렬
