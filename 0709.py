e04fa944307aee3667d1313f9fa885a345611eea
yeonjae <pyj3740@gmail.com>
2024-07-09 09:45:48 +0900

    1차 제출 후 통과

--- 0709.py
+++ 0709.py
@@ -9,3 +9,13 @@ for i in range(1, len(food)):
 # print(''.join(list(reversed(answer))))
 
 print(answer+"0"+''.join(list(reversed(answer))))
+
+
+def solution(food):
+    answer = ""
+
+    for i in range(1, len(food)):
+        answer += f"{i}"*(food[i]//2)
+
+    return answer+"0"+"".join(list(reversed(answer)))
+    
