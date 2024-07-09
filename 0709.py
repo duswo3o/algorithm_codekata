7f6c58fad20a4f9cd7d55208239035d733b63e4d
yeonjae <pyj3740@gmail.com>
2024-07-09 09:48:45 +0900

    푸드 파이트 대회

--- 0709.py
+++ 0709.py
@@ -12,10 +12,10 @@ print(answer+"0"+''.join(list(reversed(answer))))
 
 
 def solution(food):
-    answer = ""
+    answer = "" # 정답 string
 
-    for i in range(1, len(food)):
-        answer += f"{i}"*(food[i]//2)
+    for i in range(1, len(food)): # food 리스트의 첫 번째 항목부터 마지막 항목까지
+        answer += f"{i}"*(food[i]//2) # 음식의 개수를 2개로 나누어 배치
 
-    return answer+"0"+"".join(list(reversed(answer)))
+    return answer+"0"+"".join(list(reversed(answer))) # 음식 배치 + 물 + 음식배치 거꾸로
     
