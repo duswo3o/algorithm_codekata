d59c41ec959d683c24f9587fa651d3da23e6e0b4
yeonjae <pyj3740@gmail.com>
2024-07-09 09:42:27 +0900

    ㅍㅜ트 파이트 대회 초안

---
+++ 0709.py
@@ -0,0 +1,11 @@
+# 푸드 파이트 대회
+
+food = [1,3,4,6]
+
+answer = ""
+for i in range(1, len(food)):
+    answer += f"{i}"*(food[i]//2)
+
+# print(''.join(list(reversed(answer))))
+
+print(answer+"0"+''.join(list(reversed(answer))))
