54001341f60d1cae2a3ec899cfcb2d8da02ab571
yeonjae <pyj3740@gmail.com>
2024-07-10 09:28:01 +0900

    콜라문제

--- 0710.py
+++ 0710.py
@@ -1,11 +1,11 @@
 # 콜라 문제
 
 def solution(a, b, n):
-    get_bottle = 0
+    get_bottle = 0 # 받은 병의 개수
 
-    while n//a > 0:
-        get_bottle += (n//a)*b
-        n = (n//a)*b + (n%a)
+    while n//a > 0: # 더 이상 받을 수 있는 병이 없을 때까지
+        get_bottle += (n//a)*b # 받은 병의 수 계산
+        n = (n//a)*b + (n%a) # 가지고 있는 빈 병의 수 업데이트
 
     return get_bottle
 
