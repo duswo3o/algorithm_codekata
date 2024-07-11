50a4cc9bcbe21f745eace15578242de28c4e8184
yeonjae <pyj3740@gmail.com>
2024-07-11 09:29:57 +0900

    명예의 전당(1)

--- 0711.py
+++ 0711.py
@@ -1,17 +1,17 @@
 # 명예의 전당
 
 def solution(k, score):
-    hof = [score[0]]
-    result = [hof[-1]]
+    hof = [score[0]] # 명예의 전단
+    result = [hof[-1]] # 명예의 전당에서 가장 낮은 점수들을 기록
 
-    for i in range(1, len(score)):
-        hof.append(score[i])
-        hof.sort(reverse=True)
+    for i in range(1, len(score)): 
+        hof.append(score[i]) # 명예의 전당에 점수 추가
+        hof.sort(reverse=True) # 내림차순 정렬
 
-        if len(hof) > k:
-            hof.pop()
-            
-        result.append(hof[-1])
+        if len(hof) > k: # 만약 명예의 전당 길이가 k이상이 되면
+            hof.pop() # 가장 낮은 점수를 뺌
+
+        result.append(hof[-1]) # 명예의 전당에서 가잔 낮은 점수를 result에 추가
 
     return result
 
