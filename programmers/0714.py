1e56a30f14fc40b68750771582ebd5c667f7cdf3
yeonjae <pyj3740@gmail.com>
2024-07-12 15:39:30 +0900

    과일 장수

--- programmers/0714.py
+++ programmers/0714.py
@@ -1,14 +1,14 @@
 # 과일 장수
 
 def solution(k,m,score):
-    score.sort()
+    score.sort() # 오름차순 정렬
+    result = 0 # 최대 이익
 
-    result = 0
-    while len(score)>=m:
+    while len(score)>=m: # 담을 개수기 모자라지 않으면
         apple_box = []
-        for i in range(m):
+        for i in range(m): # 개의 사과를 담기(사과 점수가 높은거부터)
             apple_box.append(score.pop())
-        result += apple_box.pop() * m
+        result += apple_box.pop() * m # 박스에 담긴 가장 낮은 점수 * 사과의 개수를 이익에 더해줌
     return result
 
 print(solution(3,4,[1,2,3,1,2,3,1]))
