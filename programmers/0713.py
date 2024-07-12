61610429d3aa5ab32fe803fbac7bce91c8852718
yeonjae <pyj3740@gmail.com>
2024-07-12 11:27:46 +0900

    25번 실패

---
+++ programmers/0713.py
@@ -0,0 +1,25 @@
+# 키드뭉치
+
+from collections import deque
+def solution(cards1, cards2, goal):
+    cards1 = deque(cards1)
+    cards2 = deque(cards2)
+    goal = deque(goal)
+
+    c1, c2, g = cards1.popleft(), cards2.popleft(), goal.popleft()
+
+    while goal:
+        if g not in [c1,c2]:
+            return "No"
+
+        if c1 == g and len(cards1) > 0:
+            c1 = cards1.popleft()
+        if c2 == g and len(cards2) > 0:
+            c2 = cards2.popleft()
+        g = goal.popleft()
+
+    return "Yes"
+
+# print(solution(["i", "drink", "water"], ["want", "to"], ["i", "want", "to", "drink", "water"]))
+# print(solution(["show", "lot", "please", "the", "me"], ["money"], ["show", "me", "the", "money"]))
+print(solution(["a","b","c"],["d","e","f"],["a","d","f"])) # No
