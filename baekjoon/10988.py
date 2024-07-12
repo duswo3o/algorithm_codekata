59ef3d25a92ecc3144fb81bf1c7592fc33a3713c
yeonjae <pyj3740@gmail.com>
2024-07-12 12:00:55 +0900

    팰린드롬인지 확인하기

--- baekjoon/10988.py
+++ baekjoon/10988.py
@@ -1,5 +1,7 @@
 # 팰린드롬인지 확인하기
 
+
+# 스택 이용해서 풀기
 def is_palindrome(s):
     l = len(s) # 문자열의 길이
     front = [i for i in s[:l//2]] # 절반 나눠서 앞쪽 글자
@@ -13,5 +15,18 @@ def is_palindrome(s):
                 return 0
     return 1
 
-a = input()
-print(is_palindrome(a))
+
+
+
+
+
+# 파이썬처럼 풀기
+def is_palindrome2(s):
+    for i in range(len(s)//2):
+        if s[i] != s[-i-1]:
+            return 0
+    return 1
+
+
+
+print(is_palindrome2("abacba"))
