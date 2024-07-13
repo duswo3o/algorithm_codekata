28c1d952c5aa6dfae78267168b7590ede051e6da
yeonjae <pyj3740@gmail.com>
2024-07-13 21:42:41 +0900

    등차수열의 특정한 항만 더하기

---
+++ programmers/sum_arithmetic_sequence.py
@@ -0,0 +1,18 @@
+# 등차수열의 특정한 항만 더하기
+
+def solution(a,d,included):
+    answer = 0
+    for i in range(len(included)):
+        if included[i] == True:
+            answer += a+d*i
+    return answer
+
+
+def test_solution(a,d,included):
+    return sum([a+d*i for i in range(len(included)) if included[i]==True])
+
+
+print(solution(3,4,[True, False, False, True, True]))
+print(solution(7,1,[False, False, False, True, False, False, False]))
+print(test_solution(3,4,[True, False, False, True, True]))
+print(test_solution(7,1,[False, False, False, True, False, False, False]))
