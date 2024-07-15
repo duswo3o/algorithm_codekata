80d649f830ae096c5db3a833b15b2085ef9f4137
yeonjae <pyj3740@gmail.com>
2024-07-15 11:29:10 +0900

    정수 내림차순으로 배치하기

---
+++ programmers/0608.py
@@ -0,0 +1,6 @@
+# 정수 내림차순으로 배치하기
+
+# def solution(n):
+#     return int(''.join(sorted([i for i in str(n)], reverse=True)))
+
+solution = lambda x:int(''.join(sorted([i for i in str(n)], reverse=True)))
