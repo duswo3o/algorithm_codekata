7de0eb34d6dd47b53f1d74e0dc5202c356ae5651
yeonjae <pyj3740@gmail.com>
2024-07-15 10:33:08 +0900

    테케 3,7,8 실패

---
+++ programmers/0719.py
@@ -0,0 +1,21 @@
+# 로또의 최고 순위와 최저 순위
+
+
+def solution(lottos, win_nums):
+    lotto_rank ={0:6, 1:6, 2:5, 3:4, 4:3, 5:2, 6:1}
+    lottos = set(lottos)
+    win_nums = set(win_nums)
+
+    if len(lottos) != 6:
+        unknown_lotto = 7 - len(lottos)
+    else:
+        unknown_lotto = 0
+
+    correct_lotto = len(lottos)+len(win_nums) - len(lottos | win_nums)
+
+    return [lotto_rank[unknown_lotto+correct_lotto], lotto_rank[correct_lotto]]
+
+
+print(solution([44,1,0,0,31,25],[31,10,45,1,6,19]))
+print(solution([0,0,0,0,0,0], [38,19,20,40,15,25]))
+print(solution([45,4,35,20,3,9], [20,9,3,45,4,35]))
