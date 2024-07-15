1a9442cc0d134252edfea25e3004e57467e863f1
yeonjae <pyj3740@gmail.com>
2024-07-15 19:08:20 +0900

    카드1

--- baekjoon/2161.py
+++ baekjoon/2161.py
@@ -10,4 +10,44 @@ def card(n):
 
     return print(c.pop())
 
-card(int(input()))
+# card(int(input()))
+
+############################################################
+
+# 큐 구현해서 플어보기
+
+class Node:
+    def __init__(self, value, next):
+        self.value = value
+        self.next = next
+
+class Queue:
+    def __init__(self):
+        self.front = None
+
+    def push(self, val):
+        if self.front is None:
+            self.front = Node(val, None)
+
+        else:
+            q = self.front
+            while q.next:
+                q = q.next
+            q.next = Node(val, None)
+
+    def pop(self):
+        if self.front:
+            q = self.front
+            self.front = self.front.next
+
+            return q.value
+
+
+c = int(input())
+que = Queue()
+for i in range(c):
+    que.push(i+1)
+
+while que.front.value != None:
+    print(que.pop())
+    que.push(que.pop())
