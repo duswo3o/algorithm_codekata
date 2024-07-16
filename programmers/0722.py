4ae23ac683096cf4fbca3b0a39093a9fd0300c70
yeonjae <pyj3740@gmail.com>
2024-07-16 11:29:25 +0900

    tc 11,15,16 fail

--- programmers/0722.py
+++ programmers/0722.py
@@ -1,28 +1,30 @@
 # 체육복
 
 def solution(n, lost, reserve):
-    students = [i+1 for i in range(n)]
-    c = 0
+    no_suit = 0
+    common = set(lost)&set(reserve)
+    render = [i for i in common]
 
-    render = []
-    while students:
-        now_std = students.pop()
-        if now_std not in lost:
-            c += 1
+    while lost:
+        now_std = lost.pop()
+        if now_std in common:
+            continue
 
+        next = now_std + 1
+        pre = now_std - 1
+        if pre in reserve and pre not in render:
+            render.append(pre)
+        elif next in reserve and next not in render:
+            render.append(next)
         else:
-            next = now_std+1
-            pre = now_std-1
-            if next in reserve and next not in render:
-                render.append(next)
-                c += 1
-            elif pre in reserve and pre not in render:
-                render.append(pre)
-                c += 1
+            no_suit += 1
 
-    return c
+    return n - no_suit
+
 
 
 print(solution(5,[2,4], [1,3,5]))
 print(solution(5, [2,4],[3]))
 print(solution(3,[3], [1]))
+print(solution(5,[4,5], [3,4])) # return 4
+print(solution(5,[1,2],[2,3])) # return 4
