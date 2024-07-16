afff15f6ff1fc58838a145a991aa53ce047dbb91
yeonjae <pyj3740@gmail.com>
2024-07-16 21:46:31 +0900

    행렬 덧셈

---
+++ baekjoon/2738.py
@@ -0,0 +1,20 @@
+# 행렬 덧셈
+
+A = []
+B = []
+
+N, M = map(int, input().split())
+for _ in range(N):
+    A.append(list(map(int,input().split())))
+
+for _ in range(N):
+    B.append(list(map(int,input().split())))
+
+# 행렬 덧셈
+for n in range(N):
+    for m in range(M):
+        A[n][m]+=B[n][m]
+
+# 정답 행렬 출력
+for i in range(len(A)):
+    print(" ".join(map(str,A[i])))
