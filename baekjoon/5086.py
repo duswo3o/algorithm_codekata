839653353dc04e9b2906f2576b64833ed7197c2a
yeonjae <pyj3740@gmail.com>
2024-07-17 17:21:58 +0900

    배수와 약수

---
+++ baekjoon/5086.py
@@ -0,0 +1,14 @@
+# 배수와 약수
+
+while True:
+    command = input()
+    if command == "0 0":
+        break
+
+    a, b = map(int, command.split())
+    if a < b and b%a == 0:
+        print("factor")
+    elif a>b and a%b == 0:
+        print("multiple")
+    else:
+        print("neither")
