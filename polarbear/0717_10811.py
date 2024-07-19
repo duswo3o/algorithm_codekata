2cb625fb230ec6cc4dbfb2be8863303f1026966b
yeonjae <pyj3740@gmail.com>
2024-07-19 20:20:22 +0900

    바구니 뒤집기

--- polarbear/0717_10811.py
+++ polarbear/0717_10811.py
@@ -2,16 +2,16 @@
 
 def reverse_basket(basket, i, j):
     re_basket = []
-    for a in range(0,i-1):
+    for a in range(0, i-1):
         re_basket.append(basket[a])
-    for a in range(j-1,i-2,-1):
+    for a in range(j-1, i-2, -1):
         re_basket.append(basket[a])
-    for a in range(j,len(basket)):
+    for a in range(j, len(basket)):
         re_basket.append(basket[a])
 
     return re_basket
 
-N,M = map(int,input().split())
+N, M = map(int, input().split())
 basket = [b+1 for b in range(N)]
 for _ in range(M):
     i, j = map(int,input().split())
