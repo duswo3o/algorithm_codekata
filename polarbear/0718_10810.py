8a430dd1c7241c6bb709faba22fbd6f81162051e
yeonjae <pyj3740@gmail.com>
2024-07-19 20:23:00 +0900

    공 넣기

--- polarbear/0718_10810.py
+++ polarbear/0718_10810.py
@@ -3,8 +3,8 @@
 N, M = map(int,input().split())
 basket = ["0" for _ in range(N)]
 for _ in range(M):
-    i,j,k = map(int,input().split())
-    for b in range(i-1,j):
-        basket[b] = str(k)
+    i, j, k = map(int, input().split())
+    for b in range(i-1, j): # i번부터 j반까지 바구니들에 대해
+        basket[b] = str(k) # 바구니 업데이트
 
 print(" ".join(basket))
