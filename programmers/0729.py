c64722f27b40d711a2179b063b248d4c427ff467
yeonjae <pyj3740@gmail.com>
2024-07-19 10:18:22 +0900

    개인정보 수집 유효기간

--- programmers/0729.py
+++ programmers/0729.py
@@ -2,32 +2,33 @@
 
 
 def solution(today, terms, privacies):
-    due = {}
-    today_y, today_m, today_d = map(int, today.split("."))
+    due = {} # 약관 종류별 유효 기간
+    today_y, today_m, today_d = map(int, today.split(".")) # 오늘의 년, 월, 일
     for i in terms :
         t , term = i.split()
         due[t] = int(term)
 
-    result = []
+    result = [] # 파기할 문서 번호
     for i, p in enumerate(privacies):
-        date, t = p.split()
-        year, month, day = map(int, date.split('.'))
-        month += due[t]
+        date, t = p.split() # 개인정보 수집일자와 약관
+        year, month, day = map(int, date.split('.')) # 개인정보 수집일자의 년, 월, 일
+        month += due[t] # 유효기간 더해주기
         day -= 1
 
-        if month > 12:
-            month -= 12
-            year += 1
+        while month > 12: # 월이 12월을 넘어가지 않을 때까지
+            month -= 12 # 12월 이상이면 12를 빼주고
+            year += 1 # 1년 추가
 
-        if day == 0:
-            month -= 1
-            day = 28
-            if month == 0:
-                year -= 1
-                month = 12
+        if day == 0: # 0일인 경우
+            month -= 1 # 한 달 빼고
+            day = 28 # 모든 달은 28일까지만 존재
+            if month == 0: # 월이 0이 되는 경우
+                year -= 1 # 1년 빼기
+                month = 12 # 월은 12월
 
+        # 만료일의 년도가 오늘의 년도보다 작거나 / 같은 년도에 월이 작거나 / 같은년도 같은 월에 일이 작은 경우
         if (year < today_y) or (year == today_y and month < today_m) or (year == today_y and month == today_m and day < today_d):
-            result.append(i+1)
+            result.append(i+1) # 파기할 문서에 추가
 
     return result
 
