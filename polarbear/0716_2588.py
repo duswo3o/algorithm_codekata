c207bfae306cb9fcaf759ae174c6004980aeb76b
yeonjae <pyj3740@gmail.com>
2024-07-19 20:16:02 +0900

    곱셈

--- polarbear/0716_2588.py
+++ polarbear/0716_2588.py
@@ -1,10 +1,10 @@
 # 곱셈
 
-def vertical_multiplication(a,b):
+def vertical_multiplication(a, b):
     for i in b[::-1]: # 문자열 역순으로
         print(a*int(i))
     print(a*int(b))
 
 a = int(input())
 b = input()
-vertical_multiplication(a,b)
+vertical_multiplication(a, b)
