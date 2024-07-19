e58d855911660e0aed8af3f4b3010ec923e3525f
yeonjae <pyj3740@gmail.com>
2024-07-19 20:11:24 +0900

    조건 문자열

--- polarbear/0715_condition_string.py
+++ polarbear/0715_condition_string.py
@@ -3,13 +3,13 @@
 def solution(ineq,eq,n,m):
     eq_sign = ineq+eq #[<=,>=,<,>]
     if eq_sign == "<=":
-        answer = n<=m
+        answer = n <= m
     elif eq_sign == ">=":
-        answer = n>=m
+        answer = n >= m
     elif eq_sign == "<!":
-        answer = n<m
+        answer = n < m
     elif eq_sign == ">!":
-        answer = n>m
+        answer = n > m
     return 1 if answer == True else 0
 
 
