1fa6e356b151b49b2cc9a388b972b612e6f9499b
yeonjae <pyj3740@gmail.com>
2024-07-19 20:19:39 +0900

    과제 안 내신분..?

--- polarbear/0717_5597.py
+++ polarbear/0717_5597.py
@@ -5,6 +5,6 @@ submit_number = []
 for _ in range(28):
     submit_number.append(int(input()))
 
-not_submit = set(student_number) - set(submit_number)
+not_submit = set(student_number) - set(submit_number) # 제출 안 한 사람 찾기 : 차집합
 print(min(not_submit))
 print(max(not_submit))
