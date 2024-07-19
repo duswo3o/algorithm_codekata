6dae7c600d899f1a948edad7d68d6df69d64f9f1
yeonjae <pyj3740@gmail.com>
2024-07-19 20:10:34 +0900

    이어 붙인 수

--- polarbear/0715_concatenated_number.py
+++ polarbear/0715_concatenated_number.py
@@ -4,7 +4,7 @@ def solution(num_list):
     odd = ""
     even = ""
     for i in num_list:
-        if i%2==0:
+        if i % 2 == 0:
             even += f"{i}"
         else:
             odd += f"{i}"
