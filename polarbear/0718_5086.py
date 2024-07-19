076fa322ae101d4fa7d214ba8a434e55ea5d82f2
yeonjae <pyj3740@gmail.com>
2024-07-19 20:20:48 +0900

    배수와 약수

--- polarbear/0718_5086.py
+++ polarbear/0718_5086.py
@@ -6,7 +6,7 @@ while True:
         break
 
     a, b = map(int, command.split())
-    if a < b and b%a == 0:
+    if a < b and b % a == 0:
         print("factor")
     elif a>b and a%b == 0:
         print("multiple")
