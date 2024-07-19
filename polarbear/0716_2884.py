b730ecf0db97715a828fc30c4d94220eb2caaf5f
yeonjae <pyj3740@gmail.com>
2024-07-19 20:17:26 +0900

    알람시계

--- polarbear/0716_2884.py
+++ polarbear/0716_2884.py
@@ -1,12 +1,12 @@
 # 알람 시계
 
 def alarm_clock(hour, minute):
-    minute = minute - 45
-    if minute < 0:
-        minute += 60
-        hour -= 1
-        if hour < 0:
-            hour += 24
+    minute = minute - 45 # 분
+    if minute < 0: # 분이 마이너스 인 경우
+        minute += 60 # 분을 양수값으로 변환
+        hour -= 1 # -1시간
+        if hour < 0: # 시간이 음수인 경우
+            hour += 24 # 24를 더해줌
     return print(hour, minute)
 
 hour, minute = input().split()
