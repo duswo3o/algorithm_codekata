083dbbebfdded63a47a22641939f1d38241f09f6
yeonjae <pyj3740@gmail.com>
2024-07-23 10:54:37 +0900

    예상 대진표

--- programmers/0807.py
+++ programmers/0807.py
@@ -1,12 +1,12 @@
 # 예상 대진표
 
 def solution(N,A,B):
-    cnt = 0
+    cnt = 0 # 마주치는 대진 순서
     A, B = A-1, B-1
-    while A != B:
-        cnt += 1
-        A = A//2
-        B = B//2
+    while A != B: # A와 B가 다르면
+        cnt += 1 # 대진 순서 추가
+        A = A//2 # A 업데이트
+        B = B//2 # B 업데이트
     return cnt
 
 print(solution(8,4,7))
