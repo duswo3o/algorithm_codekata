d3bc290f2320437e03b535bc783c4bd19646ec9c
yeonjae <pyj3740@gmail.com>
2024-07-23 10:49:58 +0900

    피보나치 수

--- programmers/0805.py
+++ programmers/0805.py
@@ -1,10 +1,10 @@
 # 피보나치 수
 
 def solution(n):
-    fibo = [0,1,1]
-    for i in range(2, n):
+    fibo = [0,1,1] # 피보나치 수 배열
+    for i in range(2, n): # 피보나치 수 추가하기
         fibo.append(fibo[i-1]+fibo[i])
-    return fibo[n]%1234567
+    return fibo[n]%1234567 # 피보나치 수 출력
 
 print(solution(3))
 print(solution(5))
