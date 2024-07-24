65cac362873b411ddfdfed39c29a7678b61a18ed
yeonjae <pyj3740@gmail.com>
2024-07-24 11:50:46 +0900

    멀리 뛰기

--- programmers/0809.py
+++ programmers/0809.py
@@ -12,12 +12,13 @@ def solution(n):
     result = 0
     one, two = n, 0
     while two <= n//2:
-        result += (factorial(one+two)//(factorial(one)*factorial(two)))%1234567
+        result += (factorial(one+two)//(factorial(one)*factorial(two)))
         one -= 2
         two += 1
-    return result
+    return result%1234567
 
 print(solution(4))
 print(solution(3))
 print(solution(1))
 print(solution(2))
+print(solution(2000))
