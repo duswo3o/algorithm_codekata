6149c48cbe8e91d37701d66473129bed957a6fda
yeonjae <pyj3740@gmail.com>
2024-07-24 11:03:02 +0900

    N개의 최소공배수

--- programmers/0808.py
+++ programmers/0808.py
@@ -1,36 +1,19 @@
 # N개의 최소공배수
 
-def divisor(n):
-    div = [1]
-    for i in range(2, int(n**0.5)+1):
-        if n%i == 0:
-            div.append(i)
-    r = len(div)
-    for i in range(r):
-        div.append(n//div[i])
-    return sorted(div, reverse=True)
-
-
-def gcd(arr):
-    mini = min(arr)
-    mini_div = divisor(mini)
-    for i in mini_div:
-        l = []
-        for j in arr:
-            if j%i != 0:
-                break
-            else:
-                l.append(1)
-        if len(l) == len(arr):
-            return i
-    return 1
+def gcd(a, b):
+    while b!=0:
+        a, b = b, a%b
+    return a
 
 def solution(arr):
-    g = gcd(arr)
-    result = g
-    for i in arr:
-        result *= i//g
-    return result
+    arr.sort()
+    lmc = arr.pop()
+    while arr:
+        a = arr.pop()
+        if lmc % a != 0:
+            g = gcd(lmc, a)
+            lmc = g * (lmc//g) * (a//g)
+    return lmc
 
 print(solution([2,6,8,14]))
 print(solution([1,2,3]))
