d4d675ac8640847a8587c7a6ab63c10b6dfa0f8b
yeonjae <pyj3740@gmail.com>
2024-07-24 12:34:26 +0900

    멀리 뛰기

--- programmers/0809.py
+++ programmers/0809.py
@@ -1,5 +1,6 @@
 # 멀리 뛰기
 
+# 팩토리얼 구하기
 def factorial(n):
     fac = [1,1]
     for i in range(2,n+1):
@@ -9,10 +10,12 @@ def factorial(n):
 # print(factorial(3))
 
 def solution(n):
-    result = 0
-    one, two = n, 0
-    while two <= n//2:
+    result = 0 # 멀리뛰기의 경우의 수
+    one, two = n, 0 # 1칸과 2칸의 수
+    while two <= n//2: # 2칸의 수가 n//2보다 커지면 종료
+        # 동일한 값이 있는 순열 계산하기
         result += (factorial(one+two)//(factorial(one)*factorial(two)))
+        # 한 칸과 두 칸의 수 업데이트
         one -= 2
         two += 1
     return result%1234567
