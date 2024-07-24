91df5bf045a028cd02f7aa4f5b196c0048d08d67
yeonjae <pyj3740@gmail.com>
2024-07-24 12:32:05 +0900

    N개의 최소공배수

--- programmers/0808.py
+++ programmers/0808.py
@@ -1,18 +1,20 @@
 # N개의 최소공배수
 
+# 최대공약수 : 유클리드 호제법 이용
 def gcd(a, b):
     while b!=0:
         a, b = b, a%b
     return a
 
 def solution(arr):
-    arr.sort()
-    lmc = arr.pop()
+    arr.sort() # 오름차순 정렬
+    # 큰 수 부터 차례로 하나씩 추가하며 최소공배수 찾기
+    lmc = arr.pop() # 최소공배수
     while arr:
         a = arr.pop()
-        if lmc % a != 0:
-            g = gcd(lmc, a)
-            lmc = g * (lmc//g) * (a//g)
+        if lmc % a != 0: # 현재 최소공배수가 추가된 값의 배수가 아닌 경우
+            g = gcd(lmc, a) # 현재 최소공배수와 추가된 수의 최대공약수 찾기
+            lmc = g * (lmc//g) * (a//g) # 최소공배수 업데이트
     return lmc
 
 print(solution([2,6,8,14]))
