dc7c6c0ec619f184ee655d05cc1a79da379a0ffd
yeonjae <pyj3740@gmail.com>
2024-07-24 11:45:11 +0900

    실패

---
+++ programmers/0809.py
@@ -0,0 +1,23 @@
+# 멀리 뛰기
+
+def factorial(n):
+    fac = [1,1]
+    for i in range(2,n+1):
+        fac.append(fac[i-1]*i)
+    return fac[-1]
+
+# print(factorial(3))
+
+def solution(n):
+    result = 0
+    one, two = n, 0
+    while two <= n//2:
+        result += (factorial(one+two)//(factorial(one)*factorial(two)))%1234567
+        one -= 2
+        two += 1
+    return result
+
+print(solution(4))
+print(solution(3))
+print(solution(1))
+print(solution(2))
