3e53ab37c8c6de48bc6fa970ea78a3a7c6dbdf73
yeonjae <pyj3740@gmail.com>
2024-07-25 20:03:58 +0900

    토마토

--- baekjoon/7576.py
+++ baekjoon/7576.py
@@ -66,41 +66,44 @@
 import sys
 from collections import deque
 
+
+# 넓이 우선 탐색 : 주변의 모든 토마토를 익게 만들어서 넓이 우선 탐색 이용!
 def bfs(queue):
+    # 상하좌우 이동
     dx = [1,0,-1,0]
     dy = [0,1,0,-1]
 
     while ripe:
-        now_x, now_y = queue.popleft()
+        now_x, now_y = queue.popleft() # 현재 좌표
         for i in range(4):
-            next_x, next_y = now_x + dx[i], now_y + dy[i]
-
+            next_x, next_y = now_x + dx[i], now_y + dy[i] # 다음 방문할 좌표
+            # 좌표가 범위 내에 존재하고 다음 갈 좌표가 안익은 토마토라면
             if (0<=next_x<N) and (0<=next_y<M) and (tomato[next_x][next_y] == 0):
-                tomato[next_x][next_y] = tomato[now_x][now_y] + 1
-                queue.append((next_x, next_y))
+                tomato[next_x][next_y] = tomato[now_x][now_y] + 1 # 익는데 걸리는 날짜 값으로 업데이트
+                queue.append((next_x, next_y)) # 방문할 좌표에 추가
     return tomato
 
 
 M,N = map(int, sys.stdin.readline().split())
-tomato = []
+tomato = [] # 토마토 밭
 for _ in range(N):
     t = list(map(int, sys.stdin.readline().split()))
     tomato.append(t)
 
-ripe = deque()
-
+ripe = deque() # 익은 토마토의 위치
+# 익은 토마토 찾기
 for row in range(N):
     for col in range(M):
         if tomato[row][col] == 1:
             ripe.append((row,col))
 
-bfs(ripe)
-day = 0
+bfs(ripe) # 토마토 익는중
+day = 0 # 토마토가 익는데 걸리는 날
 for r in tomato:
     for c in range(M):
-        if r[c] == 0:
-            print(-1)
-            exit()
-    day = max(day, max(r))
+        if r[c] == 0: # 안익은 토마토가 있으면
+            print(-1) # -1 출력하고
+            exit() # 종료 -> 이후 코드는 무시
+    day = max(day, max(r)) # 익는데 가장 오래 걸리는 날짜
 
-print(day -1)
+print(day -1) # 익어있는 토마토부터 시작했으므로 -1을 해줌
