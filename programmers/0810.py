eb50e00c2e667ea8297383c87ef5e1bb6cb8ea02
yeonjae <pyj3740@gmail.com>
2024-07-25 20:45:03 +0900

    반례 하나 찾음

--- programmers/0810.py
+++ programmers/0810.py
@@ -1,5 +1,5 @@
-# 귤 고르기
-
+# # 귤 고르기
+#
 # from collections import defaultdict
 #
 # def solution(k, tangerine):
@@ -19,7 +19,9 @@
 #     cnt_list = sorted(cnt_list, reverse=True) # 내림차순 정렬
 #     tang = 0 # 상자에 담은 귤의 개수
 #
-#     # # 가장 개수가 많은 귤의 종류부터 담기
+#
+#
+#     # 가장 개수가 많은 귤의 종류부터 담기
 #     # for i in cnt_list:
 #     #     if tang >= k:
 #     #         while tang > k:
@@ -31,6 +33,7 @@
 #     #     ans += cnt_dict[i]
 #     #     tang += cnt_dict[i] * i
 #     #     j = i
+#     # return ans
 #
 #     for i in cnt_list:
 #         if tang < k:
@@ -43,6 +46,7 @@
 #                 ans -=1
 #             if tang == k: return ans
 #             else: return ans+1
+#     return ans
 
 
 ###############################################################
@@ -76,10 +80,11 @@ def solution(k, tangerine):
 
 
 
-print(solution(6, [1, 3, 2, 5, 4, 5, 2, 3])) # 3
-print(solution(4, [1, 3, 2, 5, 4, 5, 2, 3])) # 2
-print(solution(2, [1, 1, 1, 1, 2, 2, 2, 3])) # 1
-print(solution(6, [1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9])) # 4
+# print(solution(6, [1, 3, 2, 5, 4, 5, 2, 3])) # 3
+# print(solution(4, [1, 3, 2, 5, 4, 5, 2, 3])) # 2
+# print(solution(2, [1, 1, 1, 1, 2, 2, 2, 3])) # 1
+# print(solution(6, [1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9])) # 4
 print(solution(3,[1,1,2,2])) # 2
 print(solution(2,[1000,2000,2000,1000])) # 1
-print(solution(3,[1,2,5])) # 3
+# print(solution(3,[1,2,5])) # 3
+print(solution(1, [1]))
