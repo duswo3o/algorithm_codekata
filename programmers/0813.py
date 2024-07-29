6259668e8595a3cc8e04de459d4adf0266ea8985
yeonjae <pyj3740@gmail.com>
2024-07-29 10:22:44 +0900

    H-Index

--- programmers/0813.py
+++ programmers/0813.py
@@ -5,13 +5,13 @@ from collections import defaultdict
 def solution(citations):
     h_index = defaultdict(int)
     n = len(citations)
-
+    # key = 인용된 횟수, value = 인용된 논문의 수
     for cnt in citations:
         for c in range(cnt):
             h_index[c] += 1
 
     # print(h_index)
-
+    # 인용된 수와 인용된 논문의 수보다 같거나 작아지는 수 == H-Index
     for i in range(n,-1,-1):
         if i <= h_index[i]:
             return h_index[i]
