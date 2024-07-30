d4419a612ac1f04479822d71a07d97989b64233c
yeonjae <pyj3740@gmail.com>
2024-07-30 09:51:28 +0900

    의상

--- programmers/0817.py
+++ programmers/0817.py
@@ -3,13 +3,15 @@
 from collections import defaultdict
 def solution(clothes):
     my_closet = defaultdict(int)
+    # 옷의 종류 : 개수
     for c in clothes:
         my_closet[c[1]] += 1
 
     answer = 1
+    # 경우의 수 계산
     for i in my_closet.values():
         answer *= i+1
-
+    # 어떤 종류도 선택 하지 않는 경우를 빼줌
     return answer-1
 
 print(solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]]))
