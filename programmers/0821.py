10467cddc853e488ff960df08c44e20bfed569cf
yeonjae <pyj3740@gmail.com>
2024-07-31 22:53:30 +0900

    타깃 넘버

--- programmers/0821.py
+++ programmers/0821.py
@@ -1,26 +1,30 @@
 # 타깃 넘버
 
 def solution(numbers, target):
-    l = []
+    # l = []
 
     def dfs(i, total):
-        if i >= len(numbers):
-            l.append(total)
-            return l
+        cnt = 0
 
-        dfs(i + 1, total + numbers[i])
-        dfs(i + 1, total - numbers[i])
-        return l
+        if i >= len(numbers):
+            # l.append(total)
+            if total == target:
+                cnt +=1
+            return cnt
+
+        cnt += dfs(i + 1, total + numbers[i])
+        cnt += dfs(i + 1, total - numbers[i])
+        return cnt
 
     a = dfs(0,0)
     # print(len(a))
     # print(a)
 
-    cnt = 0
-    for i in a:
-        if i == target:
-            cnt+=1
-    return cnt
+    # cnt = 0
+    # for i in a:
+    #     if i == target:
+    #         cnt+=1
+    return a
 
 print(solution([1, 1, 1, 1, 1], 3)) # 5
 print(solution([4, 1, 2, 1], 4)) # 2
