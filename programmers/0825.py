168e764f05e9fc0ce162ade7bf5b9b99611ac23f
yeonjae <pyj3740@gmail.com>
2024-08-02 10:27:45 +0900

    뒤에 있는 큰 수 찾기

--- programmers/0825.py
+++ programmers/0825.py
@@ -1,21 +1,20 @@
 # 뒤에 있는 큰 수 찾기
 
-from collections import deque
-
+import heapq
 def solution(numbers):
-    answer = []
-    numbers = deque(numbers)
+    answer = [-1]*len(numbers)
+    heap = []
 
-    while numbers:
-        num = numbers.popleft()
-        rear = -1
-        for i in numbers:
-            if i>num:
-                rear = i
-                break
-        answer.append(rear)
+    for idx, num in enumerate(numbers):
+        # 힙에 값이 있고, 힙의 가장 작은 값이 현재의 값보다 작은 경우
+        while heap and heap[0][0]<num:
+            value, i = heapq.heappop(heap) # 가장 작은 값 추출
+            answer[i] = num # 가장 가까이에 있는 큰 값은 현재 값(num)
+        # 현재 값의 큰 값을 찾기 위해 현재 값을 힙에 추가
+        heapq.heappush(heap, (num,idx))
+
     return answer
 
 
 print(solution([2,3,3,5]))
-print(solution([9,1,5,3,6,2]))
+# print(solution([9,1,5,3,6,2]))
