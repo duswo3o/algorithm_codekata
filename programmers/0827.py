0c0ff3591325af30f4ac3fa0e70f88a05db34fe0
yeonjae <pyj3740@gmail.com>
2024-08-03 17:22:09 +0900

    timeout

--- programmers/0827.py
+++ programmers/0827.py
@@ -1,26 +1,34 @@
 # 숫자 변환하기
 
 def solution(x, y, n):
-    ans = []
+    ans = -1
 
     def dfs(x, y, n, cnt):
         nonlocal ans
+
+        if int(y) != y:
+            return
+
         if x >= y:
             if x == y:
                 # print(cnt)
-                ans.append(cnt)
+                if ans == -1:
+                    ans = cnt
+                else:
+                    ans = min(ans, cnt)
                 return
             else:
                 # print(-1)
                 return
 
-        dfs(x + n, y, n, cnt + 1)
-        dfs(x * 2, y, n, cnt + 1)
-        dfs(x * 3, y, n, cnt + 1)
+        dfs(x, y-n, n, cnt + 1)
+        dfs(x, y/2, n, cnt + 1)
+        dfs(x, y/3, n, cnt + 1)
 
     dfs(x,y,n,0)
-    return min(ans) if ans else -1
+    return ans
 
 print(solution(10, 40, 5))
 print(solution(10,40,30))
 print(solution(2,5,4))
+print(solution(38, 40, 2))
