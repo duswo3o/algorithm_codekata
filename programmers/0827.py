219e626e1f3457aca8df827c057c6c7e93e26389
yeonjae <pyj3740@gmail.com>
2024-08-03 18:04:41 +0900

    숫자 변환하기

--- programmers/0827.py
+++ programmers/0827.py
@@ -3,25 +3,30 @@
 from collections import deque
 
 def solution(x, y, n):
-
+    # 넓이 우선 탐색
     def bfs(x, y, n):
-        cnt = 0
-        que = deque([(cnt, y)])
+        que = deque([(0, y)]) # (변환 횟수, 업데이트된 값)
         while que:
             idx, now = que.popleft()
 
-            if now > x:
-                cnt += 1
-                a,b,c = now-n, now/2, now/3
-                if x in [now-n, now/2, now/3]:
-                    return idx+1
-                que.append((cnt, now-n))
-                if now/2 == int(now/2):
-                    que.append((cnt, now/2))
-                if now/3 == int(now/3):
-                    que.append((cnt, now/3))
-        return -1
+            if now == x: # x와 y값이 동일하게 주어진 경우
+                return idx
 
+            if now > x:
+                # 연산 진행
+                a,b,c = now-n, now/2, now/3
+                # 만약 연산을 했을 때 찾는 값이 있다면
+                if x in [a, b, c]:
+                    return idx+1 # 변환 횟수 출력
+                # 연산을 했을 때 찾는 값이 없다면
+                que.append((idx+1, a)) # (연산 횟수 추가, 업데이트된 y값)
+                # 나눗셈을 했을 땐 자연수인 경우에만 큐에 추가
+                if b == int(b):
+                    que.append((idx+1, b))
+                if c == int(c):
+                    que.append((idx+1, c))
+        # x로 변환이 불가능한 경우
+        return -1
 
     ans = bfs(x,y,n)
     return ans
@@ -29,4 +34,8 @@ def solution(x, y, n):
 print(solution(10, 40, 5))
 print(solution(10,40,30))
 print(solution(2,5,4))
-print(solution(38, 40, 2))
+print(solution(38, 40, 2)) # 1
+print(solution(3,3,3)) # 0
+print(solution(15, 50, 5)) # 2
+print(solution(8, 144, 32)) # 3
+print(solution(8, 134, 32)) # -1
