ad1ac027b36e1124ce76438aa4f837e204297f7f
yeonjae <pyj3740@gmail.com>
2024-08-05 10:07:11 +0900

    2개 이하로 다른 비트

--- programmers/0828.py
+++ programmers/0828.py
@@ -1,6 +1,5 @@
 # 2개 이하로 다른 비트
 
-
 def transfer_binary(n):
     binary = []
     while n > 0:
@@ -9,19 +8,16 @@ def transfer_binary(n):
     return binary
 
 def trans_bit(binary):
-    if len(binary) == sum(binary):
-        binary.pop()
-        binary += [0, 1]
-        return binary
-
     for idx, b in enumerate(binary):
         if b == 0:
-            if idx == 0:
-                binary[idx] = 1
-                return binary
-            else:
-                binary[idx] = 1
-                binary[idx-1] = 0
+            binary[idx] = 1
+            binary[idx-1] = 0
+            return binary
+
+    binary.pop()
+    binary += [0, 1]
+    return binary
+
 
 def make_decimal(binary):
     decimal = 0
@@ -30,12 +26,17 @@ def make_decimal(binary):
 
     return decimal
 
+
 def solution(numbers):
+
     answer = []
     for n in numbers:
-        t_binary = transfer_binary(n)
-        t_bit = trans_bit(t_binary)
-        answer.append(make_decimal(t_bit))
+        if n%2 == 0:
+            answer.append(n+1)
+        else:
+            t_binary = transfer_binary(n)
+            t_bit = trans_bit(t_binary)
+            answer.append(make_decimal(t_bit))
 
     return answer
 
