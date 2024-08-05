d59936d0fe2eae364d24e7768440fc49d2fa9b93
yeonjae <pyj3740@gmail.com>
2024-08-05 09:23:44 +0900

    fail

---
+++ programmers/0828.py
@@ -0,0 +1,38 @@
+# 2개 이하로 다른 비트
+
+
+def transfer_binary(n):
+    binary = []
+    while n > 0:
+        binary.append(n%2)
+        n //= 2
+    return binary
+
+def trans_bit(binary):
+    for idx, b in enumerate(binary):
+        if b == 0:
+            binary[idx] = 1
+            return binary
+
+    binary.pop()
+    binary += [0, 1]
+    return binary
+
+def make_decimal(binary):
+    decimal = 0
+    for idx, b in enumerate(binary):
+        decimal += b*(2**idx)
+
+    return decimal
+
+def solution(numbers):
+    answer = []
+    for n in numbers:
+        t_binary = transfer_binary(n)
+        t_bit = trans_bit(t_binary)
+        answer.append(make_decimal(t_bit))
+
+    return answer
+
+
+print(solution([2,7]))
