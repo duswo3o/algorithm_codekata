d6184090207bd016c1325b753129ac46a69b6601
yeonjae <pyj3740@gmail.com>
2024-08-07 09:46:49 +0900

    tc 1~6 fail

--- unsolved/0830.py
+++ unsolved/0830.py
@@ -17,16 +17,16 @@ def solution(numbers):
         else:
             my_number.append((number, number))
 
-    my_number.sort(key = lambda x : (x[1]), reverse=True)
+    my_number.sort(key = lambda x : (x[1], -len(x[0])), reverse=True)
     for num in my_number:
         answer += num[0]
 
     return answer
 
-print(solution([6, 10, 2]))
-print(solution([3, 30, 34, 5, 9]))
-print(solution([1,10,100,1000])) # 1101001000
-print(solution([0,0,0])) # 0
-print(solution([1000, 111, 110, 101, 100, 11, 10, 1, 0])) # 1111111101011010010000
-print(solution([101, 10, 232, 23])) # 2323210110
+# print(solution([6, 10, 2]))
+# print(solution([3, 30, 34, 5, 9]))
+# print(solution([1,10,100,1000])) # 1101001000
+# print(solution([0,0,0])) # 0
+# print(solution([1000, 111, 110, 101, 100, 11, 10, 1, 0])) # 1111111101011010010000
+# print(solution([101, 10, 232, 23])) # 2323210110
 print(solution([555, 565, 566, 55, 56, 5, 54, 544, 549])) # "5665656555555554954544"
