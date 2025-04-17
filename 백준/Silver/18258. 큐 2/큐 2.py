132d576173e40c1affa909ba4215da61d2564845
duswo3o <86733114+duswo3o@users.noreply.github.com>
2025-04-17 12:29:07 +0900

    [Silver IV] Title: 큐 2, Time: 1800 ms, Memory: 144240 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.py
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -1,71 +1,35 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.next = None
-
-class Queue:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-        self.size = 0
-
-    def push(self, value):
-        node = Node(value)
-        if self.empty():
-            self.head = node
-            self.tail = node
-        else:
-            self.tail.next = node
-            self.tail = node
-        self.size += 1
-
-    def pop(self):
-        if self.empty():
-            return -1
-
-        first = self.head.value
-        self.head = self.head.next
-        self.size -= 1
-        return first
-
-    def empty(self):
-        if self.head is None:
-            return 1
-        return 0
-
-    def front(self):
-        if self.empty():
-            return -1
-        return self.head.value
-
-    def back(self):
-        if self.empty():
-            return -1
-        return self.tail.value
-
-
+from collections import deque
 import sys
 
-N = int(sys.stdin.readline())
+queue = deque()
 
-queue = Queue()
+N = int(sys.stdin.readline())
 for _ in range(N):
     _input = sys.stdin.readline().strip().split()
     order = _input[0]
+    length = len(queue)
+
     if order == "push":
-        queue.push(_input.pop())
-
+        queue.append(_input.pop())
     elif order == "pop":
-        sys.stdout.write(str(queue.pop())+"\n")
-
-    elif order == "empty":
-        sys.stdout.write(str(queue.empty())+"\n")
-
+        if length:
+            print(queue.popleft())
+        else:
+            print(-1)
     elif order == "size":
-        sys.stdout.write(str(queue.size)+"\n")
-
+        print(len(queue))
+    elif order == "empty":
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
     elif order == "front":
-        sys.stdout.write(str(queue.front())+"\n")
-
+        if length:
+            print(queue[0])
+        else:
+            print(-1)
     elif order == "back":
-        sys.stdout.write(str(queue.back())+"\n")
+        if length:
+            print(queue[-1])
+        else:
+            print(-1)
